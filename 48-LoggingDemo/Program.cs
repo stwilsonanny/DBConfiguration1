0b118b34385420913d38be076984b1b2cc1d6267
ext_beyondcentsqa09 <Beyondcent_Sqa09@trendmicro.com>
2023-06-21 17:33:11 +0800

    logger

---
+++ 48-LoggingDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace _48_LoggingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection ssc = new ServiceCollection();
+            ssc.AddLogging(logBuilder=>{
+                logBuilder.AddConsole();
+                logBuilder.SetMinimumLevel(LogLevel.Debug);
+            });
+            ssc.AddScoped<Test1>();
+
+            using (var sp = ssc.BuildServiceProvider()) {
+
+                Test1 t =  sp.GetRequiredService<Test1>();
+                t.Test();
+            }
+
+        }
+    }
+}
---
+++ 48-LoggingDemo/Test1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48_LoggingDemo
+{
+    class Test1
+    {
+        private readonly ILogger<Test1> logger;
+        public Test1(ILogger<Test1> logger) {
+            this.logger = logger;
+        }
+
+        public void Test() {
+            logger.LogDebug("开始执行数据库同步");
+            logger.LogDebug("链接数据库成功");
+            logger.LogWarning("查找数据失败，重试第一次");
+            logger.LogWarning("查找数据失败，重试第二次");
+            logger.LogError("查找数据失败");
+
+            try {
+                File.ReadAllText("A:/1.txt");
+                logger.LogDebug("读写成功");
+                        
+                
+            } catch(Exception ex){
+                logger.LogError(ex, "读取文件失败");
+            
+            }
+            
+        }
+    }
+}
