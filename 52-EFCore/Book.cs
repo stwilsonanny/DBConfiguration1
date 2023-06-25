43d0804399563fae87254f7b8fed938e17f4a48e
ext_beyondcentsqa09 <Beyondcent_Sqa09@trendmicro.com>
2023-06-25 17:15:34 +0800

    52-EFCore

---
+++ 52-EFCore/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_EFCore
+{
+    public class Book
+    {
+        public long Id { set; get; }
+        public string Title { set; get; }
+        public DateTime PubTime { set; get; }
+        public Double Price { set; get; }
+
+    }
+}
---
+++ 52-EFCore/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_EFCore
+{
+    public class MyContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Person> Persoons { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            string connectionString = "Data Source=.;Initial Catalog=Test;User ID=DDCI;Password=DDCI#PWD";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }
+
+    }
+}
